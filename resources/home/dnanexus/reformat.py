c4b283a3c44f8c562d5289c5ccc31719678dcd8b
Nadia Mohammed <nadia.mohammed@addenbrookes.nhs.uk>
2022-03-22 11:00:19 +0000

    rows added to for loop, else statement for unknown and none reported samples as NA

--- resources/home/dnanexus/reformat.py
+++ resources/home/dnanexus/reformat.py
@@ -204,8 +204,11 @@ def matching_sexes(data):
     # If reported sex is not unknown or none, then see if reported and 
     # predicted sex is a match (false/true boolean)
     for idx, row in data.iterrows():
-        if not (row['original_pedigree_sex'] == "unknown" or row['original_pedigree_sex'] == "none"):
+        if not (row['original_pedigree_sex'] == "unknown" or row['original_pedigree_sex'] == "none" or row['Predicted_Sex'] == "unknown"):
             data.at[idx, 'Match_Sexes'] = row['original_pedigree_sex'] == row['Predicted_Sex']
+        else:
+            if (row['original_pedigree_sex'] == "unknown" or row['Predicted_Sex'] == "unknown" or row['original_pedigree_sex'] == "none"):
+                data.at[idx, 'Match_Sexes'] = "NA"
             # need to make the false/true boolean to string to make it 
             # lower case for multiqc
             data.at[idx, 'Match_Sexes'] = str(data.at[idx, 'Match_Sexes']).lower()
